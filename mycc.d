0adbc7ccd00cd050e6bb94520ee45c610248da80
foh-k <fusukekashiwa@gmail.com>
2020-09-07 07:23:04 +0900

    トークナイザの実装

--- mycc.d
+++ mycc.d
@@ -1,34 +1,95 @@
-import std.stdio;
-import std.conv;
+import std.stdio; // writeln, writefln
+import std.conv; // text
+import std.container; // SList 
+import std.array; // split, join
+import std.algorithm.iteration; // map
+import std.ascii; // isDigit
+
+enum TokenKind
+{
+    operatorToken,
+    numberToken,
+    eofToken
+}
+
+struct Token
+{
+    TokenKind kind;
+    int val;
+    char op;
+}
+
+SList!Token tokenize(string pat)
+{
+    auto ret = SList!Token();
+
+    // 空白除去
+    auto p = pat.split().map!"text(a)".join();
+
+    while (p.length > 0)
+    {
+        if (p[0].isPunctuation())
+        {
+            ret.insertAfter(ret[], Token(TokenKind.operatorToken, 0, parse!char(p)));
+        }
+        else if (p[0].isDigit())
+        {
+            ret.insertAfter(ret[], Token(TokenKind.numberToken, parse!int(p), ' '));
+        }
+        else
+        {
+            stderr.writeln("トークナイズできません");
+            assert(0);
+        }
+    }
+
+    ret.insertAfter(ret[], Token(TokenKind.eofToken, 0, ' '));
+    return ret;
+}
 
 void main(string[] args)
 {
     if (args.length != 2)
     {
         stderr.writeln("引数の個数が正しくありません");
-        return;
+        assert(0);
     }
+
+    auto token = args[1].tokenize();
 
     writeln(".intel_syntax noprefix");
     writeln(".globl main");
     writeln("main:");
-    writefln("  mov rax, %s", parse!int(args[1]));
+    writefln("  mov rax, %s", token.front().val);
+    token.removeFront();
 
-    while (args[1].length > 0)
+    while (!token.empty())
     {
-        auto op = parse!char(args[1]);
-        if (op == '+')
+        auto t = token.front();
+        token.removeFront();
+        if (t.kind == TokenKind.operatorToken && t.op == '+')
         {
-            writefln("  add rax, %s", parse!int(args[1]));
+            t = token.front();
+            token.removeFront();
+
+            writefln("  add rax, %s", t.val);
+
         }
-        else if (op == '-')
+        else if (t.kind == TokenKind.operatorToken && t.op == '-')
         {
-            writefln("  sub rax, %s", parse!int(args[1]));
+            t = token.front();
+            token.removeFront();
+
+            writefln("  sub rax, %s", t.val);
+        }
+        else if (t.kind == TokenKind.eofToken)
+        {
+            break;
         }
         else
         {
-            // Error
-            stderr.writeln("予期しない入力です");
+            stderr.writeln("予期しない演算子");
+            assert(0);
         }
     }
 
--- test.sh
+++ test.sh
@@ -16,6 +16,7 @@ assert(){
     fi
 }
 
-assert 21 5+20-4
+assert 24 "5 + 20 -              4 + 6      -3"
+assert 41 " 12 + 34 - 5 "
 
 echo OK
